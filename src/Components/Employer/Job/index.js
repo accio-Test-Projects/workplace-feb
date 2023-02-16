21eb82153efd1a1b5eb32e318f9eedcef88934a6
sahil jaryal <sahil199926@gmail.com>
2023-02-16 21:58:21 +0530

    side bar css

--- src/Components/Employer/Job/index.js
+++ src/Components/Employer/Job/index.js
@@ -1,8 +1,47 @@
+import { Grid } from '@mui/material'
 import React from 'react'
-
+import Form from './Form'
+import Sidebar from './Sidebar'
 function Job() {
+  const [mobileViewForm, setMobileViewForm] = React.useState(false);
+const [selectedJob, setSelectedJob] = React.useState(null);
+  const postAjobBtn = () => {
+    setMobileViewForm(true)
+  }
+const selectedJobfun = (item) => {
+  setMobileViewForm(true)
+  setSelectedJob(item)
+  console.log(item)
+}
   return (
-    <div>Job</div>
+    <Grid
+    
+    sx={{
+      margin: 'auto',
+      maxWidth: { xs: '95%', md: '100%'}
+
+    }}
+    container >
+      <Grid
+      sx ={{
+        display: { xs: mobileViewForm?'none':'block', md: 'block' },
+        background:' #fff',
+        padding: '12px'
+      }}
+      
+      item xs={12} md={4}>
+        <Sidebar postAjobBtn={postAjobBtn} selectedJob={selectedJob} selectedJobfun={selectedJobfun} />
+        </Grid>
+      <Grid
+      
+      sx ={{
+        display: { xs: mobileViewForm?'block':'none', md: 'block' },
+      }}
+
+      item xs={12} md={8}>
+        <Form  setMobileViewForm={setMobileViewForm} />
+        </Grid>
+      </Grid>
   )
 }
 
